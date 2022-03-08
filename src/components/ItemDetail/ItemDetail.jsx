9e261f4645998f276ccb8748a8bb1eb50899e455
asotelo <albertogsotelo9@gmail.com>
2022-03-08 01:56:51 -0300

    último arreglo de 7ma entrega

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -3,7 +3,11 @@
 const ItemDetail = ({producto}) => {
     return (
         <div>
-            {producto.name}
+            <h1>Detalle de Compra</h1>
+            <p>Producto: {producto.name}</p><br></br>
+            <p>Descripción: {producto.description}</p><br></br>
+            <p>Stock disponible: {producto.stock}</p><br></br>
+            <p>ID de producto: {producto.id}</p>
         </div>
     )
 } 
