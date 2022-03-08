397ef0c8441f16b86c51f469241e2121bf7a9652
asotelo <albertogsotelo9@gmail.com>
2022-03-08 02:03:39 -0300

    + 7

--- src/components/Containers/ItemDetailContainer.jsx
+++ src/components/Containers/ItemDetailContainer.jsx
@@ -5,17 +5,21 @@ import ItemDetail from '../ItemDetail/ItemDetail'
 
 const ItemDetailContainer = () => {
     const [producto, setProducto] = useState({})
+    const [loading, setLoading]= useState([true])
     useEffect(() => {
         getFetch
         .then(resp => setProducto(resp.find(prod=> prod.id === 1)))
+        .catch(err => console.log(err))
+        .finally(() => setLoading(false))
     })
 
     console.log(producto)
     return (
         <>
-        
+            {loading ? <h1>Cargando Compra</h1>
+            :
                 
-            <ItemDetail producto={producto}/>
+            <ItemDetail producto={producto}/>}
         
         
         </>
